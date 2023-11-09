b04c7aef2262f6762fbc4fb000ee01c0aab9eb4a
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-09 16:37:33 +0200

    added the rest of navigational routes to sidebar navigation

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import {HiOutlineCalendarDays, HiOutlineCog6Tooth, HiOutlineHome, HiOutlineHomeModern, HiOutlineUsers} from 'react-icons/hi2'
 
 const NavList = styled.ul`
   display: flex;
@@ -50,8 +51,32 @@ function MainNav(){
   return(
     <nav>
       <NavList>
-        <li><StyledNavLink to="/dashboard">Home</StyledNavLink></li>
-        <li><StyledNavLink to="/bookings">Bookings</StyledNavLink></li>
+        <li>
+          <StyledNavLink to="/dashboard">
+            <HiOutlineHome/>
+            <span>Home</span>
+          </StyledNavLink>
+        </li>
+        <li><StyledNavLink to="/bookings">
+          <HiOutlineCalendarDays/>
+          <span>Bookings</span>
+          </StyledNavLink>
+        </li>
+        <li><StyledNavLink to="/cabins">
+          <HiOutlineHomeModern/>
+          <span>Cabin</span>
+          </StyledNavLink>
+        </li>
+        <li><StyledNavLink to="/users">
+          <HiOutlineUsers/>
+          <span>Users</span>
+          </StyledNavLink>
+        </li>
+        <li><StyledNavLink to="/settings">
+          <HiOutlineCog6Tooth/>
+          <span>Settings</span>
+          </StyledNavLink>
+        </li>
       </NavList>
     </nav>
   )
