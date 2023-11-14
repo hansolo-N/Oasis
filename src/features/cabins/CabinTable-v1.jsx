fb32f58bab017277f70b918134d503b4df054781
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-11-14 16:59:08 +0200

    refactored fetching cabin data as custom hook

--- src/features/cabins/CabinTable-v1.jsx
+++ src/features/cabins/CabinTable-v1.jsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-
+import { getCabins } from "../../services/ApiCabins";
 import { useQuery } from "@tanstack/react-query";
 import Spinner from '../../ui/Spinner'
 import CabinRow from './CabinRow'
-import { useCabins } from "./useCabins";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -31,9 +30,10 @@ const TableHeader = styled.header`
 
 
 function CabinTable() {
-  const {isLoading,cabins} = useCabins()
-
-
+  const {isLoading,data:cabins,error} = useQuery({
+    queryKey: ['cabins'],
+    queryFn: getCabins
+  })
   if(isLoading) return <Spinner/>
 
   return <Table role="table">
